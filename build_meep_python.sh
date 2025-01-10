524b219f5f40fb0931237c67facbfe72bad249e1
theogdoctorg <garrisoncrouch@gmail.com>
2025-01-10 13:19:37 -0800

    updating apt-get packages and Dockerfile syntax

--- build_meep_python.sh
+++ build_meep_python.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+h#!/bin/bash
 
 set -e
 
@@ -7,36 +7,36 @@ MY_LDFLAGS="-L/usr/local/lib -L/usr/lib/x86_64-linux-gnu/hdf5/serial ${RPATH_FLA
 MY_CPPFLAGS="-I/usr/local/include -I/usr/include/hdf5/serial"
 
 apt-get update
-apt-get -y install libblas-dev liblapack-dev libgmp-dev swig libgsl-dev autoconf pkg-config libpng16-dev git guile-2.0-dev libhdf5-dev hdf5-tools libfftw3-dev libpython3.5-dev python3-numpy python3-h5py
+apt-get -y install build-essential gfortran libblas-dev liblapack-dev libgmp-dev swig libgsl-dev autoconf pkg-config libpng-dev git guile-2.2 guile-2.2-dev libhdf5-dev hdf5-tools libfftw3-dev libpython3-dev python3-numpy python3-h5py python3-pip cmake
 
 mkdir -p ~/install
 
 cd ~/install
-git clone https://github.com/stevengj/harminv.git
+git clone https://github.com/NanoComp/harminv.git
 cd harminv/
 sh autogen.sh --enable-shared
 make && make install
 
 cd ~/install
-git clone https://github.com/stevengj/libctl.git
+git clone https://github.com/NanoComp/libctl.git
 cd libctl/
 sh autogen.sh --enable-shared
 make && make install
 
 cd ~/install
-git clone https://github.com/stevengj/h5utils.git
+git clone https://github.com/NanoComp/h5utils.git
 cd h5utils/
 sh autogen.sh LDFLAGS="${MY_LDFLAGS}" CPPFLAGS="${MY_CPPFLAGS}"
 make && make install
 
 cd ~/install
-git clone https://github.com/stevengj/mpb.git
+git clone https://github.com/NanoComp/mpb.git
 cd mpb/
 sh autogen.sh --enable-shared LDFLAGS="${MY_LDFLAGS}" CPPFLAGS="${MY_CPPFLAGS}"
 make && make check && make install
 
 cd ~/install
-git clone https://github.com/stevengj/meep.git
+git clone https://github.com/NanoComp/meep.git
 cd meep/
 sh autogen.sh --enable-shared PYTHON=python3 LDFLAGS="${MY_LDFLAGS}" CPPFLAGS="${MY_CPPFLAGS}"
 make && make check && make install
